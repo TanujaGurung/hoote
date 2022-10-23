41368ea64ef6958f94d140210c64c713ddbcf1f6
dsdeepaksingh <deepak_singh@softprodigy.com>
2022-10-23 17:32:36 +0530

    added missing meta tags

--- pages/index.js
+++ pages/index.js
@@ -273,6 +273,9 @@ const Home=({resData})=> {
     <div>
       <Head>
         <title>{metaTitle}</title>
+        <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:url" content="https://team-place.com/" />
         <meta property="og:type" content="website" />
         <meta property="fb:app_id" content="2747726002141483" />
